5a9c60c3ef9b2df89450cd1709dfe64314771942
Ashik <helloashikul4@gmail.com>
2023-05-23 16:48:18 +0600

    banner done

--- src/pages/shared/navbar/NavBar.jsx
+++ src/pages/shared/navbar/NavBar.jsx
@@ -26,8 +26,8 @@ const NavBar = () => {
 
     return (
         <>
-           <div className="navbar bg-base-100">
-  <div className="navbar-start">
+           <div className="navbar fixed z-20 max-w-screen-xl bg-opacity-30 bg-black text-white ">
+  <div className="navbar-start ">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -36,7 +36,7 @@ const NavBar = () => {
        {menuOptions}
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
+    <a className="btn btn-ghost normal-case text-xl ">Bistro Boss</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
