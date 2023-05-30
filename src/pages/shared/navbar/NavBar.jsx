c53676edf540d67841c21b5ee3833962611700bf
Ashik465 <helloashikul@gmail.com>
2023-05-30 18:35:59 +0600

    add to cart by tanststack query

--- src/pages/shared/navbar/NavBar.jsx
+++ src/pages/shared/navbar/NavBar.jsx
@@ -2,9 +2,12 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import {FaShoppingCart } from 'react-icons/fa';
 import { AuthContext } from "../../../providers/AuthProvider";
+import useCart from "../../../hooks/useCart";
 
 const NavBar = () => {
   const { logOut, user } = useContext(AuthContext);
+  const [cart] = useCart()
+
   // logout
   const handleLogOut = () => {
     logOut()
@@ -32,7 +35,7 @@ const NavBar = () => {
         <Link to="/">
           <button className="btn gap-2">
              <FaShoppingCart></FaShoppingCart>
-            <div className="badge badge-secondary">+99</div>
+            <div className="badge badge-secondary">+{cart?.length | 0 }</div>
           </button>
         </Link>
       </li>
