b92262829008a8475a2df8ad513dcfa20fa1680d
Ashik <helloashikul4@gmail.com>
2023-06-02 02:56:14 +0600

    dashboard all user

--- src/layout/DashBoardLayout.jsx
+++ src/layout/DashBoardLayout.jsx
@@ -1,10 +1,11 @@
-import { FaCalendar, FaHamburger, FaHome, FaHotdog, FaShoppingCart, FaUser, FaWallet } from "react-icons/fa";
+import { FaBook, FaCalendar, FaHamburger, FaHome, FaHotdog, FaShoppingCart, FaUser, FaUtensils, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 
 const DashBoardLayout = () => {
     const [cart] = useCart()
+    const isAdmin = true ;
     return (
         <>
            <div className="drawer drawer-mobile ">
@@ -19,13 +20,18 @@ const DashBoardLayout = () => {
     <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
     <ul className="menu p-4 w-80 bg-[#D1A054] text-base-content">
       {/* <!-- Sidebar content here --> */}
-      <li><NavLink to='/dashboard/home'> <FaUser></FaUser> User Home</NavLink></li>
+      { isAdmin? <><li><NavLink to='/dashboard/home'> <FaHome></FaHome> Admin Home</NavLink></li>
+      <li><NavLink to='/dashboard/reservation'> <FaUtensils></FaUtensils> Add Item </NavLink></li>
+      <li><NavLink to='/dashboard/history'> <FaWallet></FaWallet> Manage Item </NavLink></li>
+      <li><NavLink to='/dashboard/history'> <FaBook></FaBook> Manage Bookings</NavLink></li>
+      <li><NavLink to='/dashboard/allusers'> <FaUser></FaUser> All User </NavLink></li>
+
+      </> : <><li><NavLink to='/dashboard/home'> <FaUser></FaUser> User Home</NavLink></li>
       <li><NavLink to='/dashboard/reservation'> <FaCalendar></FaCalendar> Reservation</NavLink></li>
       <li><NavLink to='/dashboard/history'> <FaWallet></FaWallet> Payment history</NavLink></li>
       <li><NavLink to='/dashboard/mycart'> <FaShoppingCart></FaShoppingCart> My cart
       <div className="badge badge-secondary">+{cart?.length | 0 }</div>
-      
-      </NavLink></li>
+       </NavLink></li></>}
       <div className="divider"></div>
       <li><NavLink to='/'> <FaHome></FaHome> Home</NavLink></li>
       <li> <NavLink to="/menu"> <FaHamburger></FaHamburger>   Our Menu</NavLink> </li>
